fbfc12870c39b9713ce3cb31b6a898017ff47d21
Robert Aaron Urbina Ruiz <aaronurbina1525@gmail.com>
2022-01-17 03:26:54 -0500

    agregado stopper al intervalo

--- packages/client/src/App/App.tsx
+++ packages/client/src/App/App.tsx
@@ -2,14 +2,93 @@
 /* eslint-disable no-console */
 import * as React from "react";
 
-import Card from "./components/Card";
+import image1 from "../assets/product1.webp";
+import image2 from "../assets/product2.webp";
+import image3 from "../assets/product3.webp";
+
 import { products } from "./constants";
+import CardList from "./components/CardList";
+
+type product = {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+};
+type impression = {
+  id: string;
+  impressions: number;
+};
 
 const App: React.FC = () => {
+  const [impressions, setImpressions] = React.useState<Array<impression>>([]);
+  const [isLoading, setIsLoading] = React.useState<boolean>(true);
+  const [keepFetching, setKeepFetching] = React.useState<number>(0);
+  const images = [image1, image2, image3];
+
+  const fetchProduct = async (id: number) => {
+    const options = { method: "GET" };
+    const response = await fetch(
+      "http://localhost:8000/status/" + String(id),
+      options
+    );
+    const data = await response.json();
+
+    return data;
+  };
+
+  const fetchProducts = (products: Array<product>) => {
+    console.log(keepFetching);
+
+    const promesas = products.map((product: product) => {
+      return fetchProduct(product.id);
+    });
+
+    Promise.all(promesas)
+      .then((resultados) => {
+        setImpressions(resultados);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.log("Error", error);
+      });
+  };
+
+  React.useEffect(
+    () => {
+      handleClick();
+    } /* eslint-disable-line */,
+    []
+  );
+
+  const handleClick = () => {
+    if (keepFetching) {
+      clearInterval(keepFetching);
+      setKeepFetching(0);
+
+      return;
+    }
+
+    const newIntervalId = setInterval(() => fetchProducts(products), 5000);
+
+    setKeepFetching(newIntervalId);
+  };
+
   return (
     <>
       <h1>Admin de Productos</h1>
-      <main className="shelf-container">{/* Inserte aquí las tarjetas */}</main>
+      <main className="shelf-container">
+        {isLoading ? (
+          <p>Cargando...</p>
+        ) : (
+          <CardList
+            handleClick={handleClick}
+            images={images}
+            impressions={impressions}
+            products={products}
+          />
+        )}
+      </main>
     </>
   );
 };
--- packages/client/src/App/components/Card.tsx
+++ packages/client/src/App/components/Card.tsx
@@ -1,28 +1,49 @@
 /* eslint-disable prettier/prettier */
 import * as React from "react";
 
-const Card: React.FC = () => {
+type product = {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+};
+
+type impression = {
+  id: string;
+  impressions: number;
+};
+
+const Card: React.FC<{
+  impression: impression;
+  product: product;
+  image: any;
+  handleClick: any;
+}> = ({ impression, product, image, handleClick }) => {
+  const [isOpen, setIsOpen] = React.useState<boolean>(false);
+
+  const handleClickCard = () => {
+    handleClick();
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div className="card">
+    <div className="card" onClick={handleClickCard}>
       <div className="card-header">
-        <div className="title">{/* Inserte aquí el nombre del producto */}</div>
+        <div className="title">{product.name}</div>
       </div>
 
       <div className="card-content">
-        <img alt="Jean" className="picture" />
+        <img alt="Jean" className="picture" src={image} />
 
-        <div>
-          <p className="product-status">
-            ✨ Interés en el producto:{" "}
-            {/* Inserte aquí las impresiones del producto */}% ✨
-          </p>
-          <p className="product-description">
-            {/* Inserte aquí la descripción del producto */}
-          </p>
-          <h2 className="product-price">
-            $ {/* Inserte aquí el precio del producto */}
-          </h2>
-        </div>
+        {isOpen && (
+          <div>
+            <p className="product-status">
+              ✨ Interés en el producto: {impression.impressions}% ✨
+            </p>
+            <p className="product-description">{product.description}</p>
+            <h2 className="product-price">$ {product.price}</h2>
+          </div>
+        )}
       </div>
     </div>
   );
